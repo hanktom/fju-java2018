bc91c3f0e55aa7eb58ddd6aa9d1544353ecb4102
user <user@es302nt>
2018-10-02 11:05:13 +0800

    two

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -5,6 +5,16 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		int age;
+		age = 19;
+		short num;
+		num = 8;
+		age = num;
+		float weight =52.5f;
+		System.out.println(age);
+		System.out.println(num);
+		System.out.println(weight);
+		
 
 	}
 
