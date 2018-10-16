eb8b08a9549b0f7a8deea81a85cc83380f24fa13
Hank Tom <hanktom@gmail.com>
2018-10-16 10:35:37 +0800

    bad, wrong

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -3,11 +3,21 @@ package com.tom;
 public class Hello {
 
 	public static void main(String[] args) {
+		//
+//		System.out.println("Hank 70 90 80");
+		String name = "Hank";
+		int english = 70;
+		int math = 90;
+		System.out.println(name + "\t" + english + "\t" + math + "\t" + (english+math)/2);
+		
+		
 		Person p = new Person(65.5f, 1.7f);
-//		p.weight = 65.5f;
-//		p.height = 1.7f;
 		System.out.println(p.bmi());
 		p.hello();
+		
+//		p.weight = 65.5f;
+//		p.height = 1.7f;
+		
 //		Person hank = new Person();
 //		Person jack = null;
 //		jack.hello();
